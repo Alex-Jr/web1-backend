1748f1b68e3f9a6589891cb0a3654d83de460f95
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2021-04-17 11:04:29 -0700

    Cadastro funcionando

--- routes/post/cadastro.js
+++ routes/post/cadastro.js
@@ -1,5 +1,30 @@
+const cadastroValidator = require("../../validator/cadastro");
+const database = require("../../database/setup");
+
 module.exports = (req, res, body) => {
-    console.log('Rota post cadastro chamada')
-    console.log(body);
-    res.end('ok');
+    console.log('Rota post cadastro', body)
+    try {
+        cadastroValidator(body);
+
+        const {
+            nome,
+            email,
+            datanasc,
+            cpf,
+            senha,
+            telefone,
+        } = body;
+
+        const sql = 'insert into usuario(nome, email, senha, cpf, data_nasc, fone) values (?, ?, ?, ?, ?, ?)';
+        const values = [ nome, email, senha, cpf, datanasc, telefone.replace('-', '')]; 
+
+        database.query(sql, values, (errors, results, fields ) => {
+            console.log(errors, results);
+            res.end('ok');
+        });
+    } catch (err) {
+        console.warn(err);
+        res.end('Falha');
+    }
+    
 }
