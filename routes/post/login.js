325e80cd84e2dcbc4e5c97d01634293050caa90e
Alex-Jr <jr.alexmendes@gmail.com>
2021-04-17 11:53:00 -0700

    Front se comunicando com o back
    Co-authored-by: CaioLr <CaioLr@users.noreply.github.com>
    Co-authored-by: LorrannNC <LorrannNC@users.noreply.github.com>

--- routes/post/login.js
+++ routes/post/login.js
@@ -1,6 +1,14 @@
 const loginValidator = require("../../validator/login");
 const database = require("../../database/setup");
 
+const { ServerResponse } = require('http')
+
+/**
+ * 
+ * @param {*} req 
+ * @param {ServerResponse} res 
+ * @param {*} body 
+ */
 module.exports = (req, res, body) => {
     console.log('Rota post login', body)
     try {
@@ -12,11 +20,12 @@ module.exports = (req, res, body) => {
         } = body;
 
         const sql = 'select * from usuario where email = ? and senha = ?';
-        const values = [ email, senha]; 
+        const values = [email, senha]; 
 
         database.query(sql, values, (errors, results, fields ) => {
             console.log(errors, results);
-            res.end('ok');
+            res.statusCode = 200;
+            res.end(JSON.stringify({user: results[0]}))
         }); 
     } catch (err) {
         console.warn(err);
