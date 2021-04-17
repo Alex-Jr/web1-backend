e78cc2e6ddc9f00c16676ff7545c811c87def0af
Alex-Jr <jr.alexmendes@gmail.com>
2021-04-17 14:29:58 -0700

    Delete route
    Co-authored-by: CaioLr <CaioLr@users.noreply.github.com>
    Co-authored-by: LorrannNC <LorrannNC@users.noreply.github.com>

--- routes/delete/usuarios.js
+++ routes/delete/usuarios.js
@@ -1,17 +1,15 @@
 const database = require("../../database/setup")
 
 module.exports = (req, res, params) => {
-    let sql = 'select * from usuario'
-    let values = []
-    if(params) {
-        sql += ' where nome like ?';
-        values = `%${params.nome.split('%20').join(' ')}%`;
-    }
+    console.log('Requested delete usuarios');
 
+    const sql = 'delete from usuario where id = ?';
+    const values = [JSON.parse(req.headers.cookie.user).id];
+   
     database.query(sql, values,(errors, results, fields) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
 
-        res.end(JSON.stringify(results));
+        res.end('ok');
     })
 }
---
+++ utils/parseCookie.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @param {String} cookies 
+ * @returns 
+ */
+module.exports = (cookies) => {
+    // user={"id":1,"nome":"alex","email":"alex@gmail.com","senha":"123456","fone":"123456789","data_nasc":"2021-04-15T07:00:00.000Z","cpf":"98884025796"}; logged=true
+    
+    if(!cookies) return {};
+
+    const cookiesArray = cookies.split('; ')
+    const parsedCookies = {};
+    
+    if(cookiesArray.length > 0) {
+        cookiesArray.forEach((cookie) => {
+            const [key, value] = cookie.split('=')
+            parsedCookies[key] = value;
+        })
+    }
+        
+    return parsedCookies;
+}
